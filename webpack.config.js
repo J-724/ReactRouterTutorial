af7ee122de54848d09431beececb241c83fb5160
J-724 <jjpernian@gmail.com>
2022-09-30 11:50:48 -0400

    Clean out to start midudev tutorial

--- webpack.config.js
+++ webpack.config.js
@@ -13,6 +13,16 @@ module.exports = {
     clean: true,
     assetModuleFilename: '[name][ext]',
   },
+  devServer: {
+    static: {
+      directory: path.resolve(__dirname, 'dist'),
+    },
+    port: 3000,
+    open: true,
+    hot: true,
+    compress: true,
+    historyApiFallback: true,
+  },
   devtool: 'source-map',
   module: {
     rules: [
